05c998cb98b311d9587d7dfaab90735b1740cfa9
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-26 16:35:52 -0500

    Link fighters in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "landing#index"
   
-  get("/fighters" => "user#index")
+  get("/fighters" => "user#index", as: :users)
   get("/fighters/:username" => "user#show", as: :user) 
   
 end
