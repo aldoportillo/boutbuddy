523986c17d8bafeec80fc82619d8d74a879858fa
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-06 10:26:43 -0600

    Add role column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ApplicationRecord
   has_many :accepted_bouts, class_name: "Bout", foreign_key: "blue_corner_id"
   has_many :messages, class_name: "Message", foreign_key: "user_id"
   
+  enum role: {admin: "admin", fighter: "fighter", promoter: "promoter", sample:}
   #TODO: Point to weightclass ID on signup
 
   def weight_class
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_192210) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_154449) do
   create_table "bouts", force: :cascade do |t|
     t.integer "red_corner_id"
     t.integer "blue_corner_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_192210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
