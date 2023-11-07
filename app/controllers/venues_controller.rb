40a7976487130ea8448df6f3bfc86ae2fa3a321d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-07 11:14:31 -0600

    Integrate GMAPS API to store lat and lng in venue table

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,8 @@
 class VenuesController < ApplicationController
+  require 'httparty'
+
   before_action :set_venue, only: %i[ show edit update destroy ]
+  
 
   # GET /venues or /venues.json
   def index
@@ -21,16 +24,26 @@ class VenuesController < ApplicationController
 
   # POST /venues or /venues.json
   def create
-    @venue = Venue.new(venue_params)
 
-    respond_to do |format|
-      if @venue.save
-        format.html { redirect_to venue_url(@venue), notice: "Venue was successfully created." }
-        format.json { render :show, status: :created, location: @venue }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @venue.errors, status: :unprocessable_entity }
+    @venue = Venue.new(venue_params)
+    lat, lng = get_coordinates_from_address(@venue.address)
+
+    if lat && lng
+      @venue.lat = lat
+      @venue.lng = lng
+
+      respond_to do |format|
+        if @venue.save
+          format.html { redirect_to venue_url(@venue), notice: "Venue was successfully created." }
+          format.json { render :show, status: :created, location: @venue }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @venue.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      @venue.errors.add(:address, "could not be geocoded")
+      render :new
     end
   end
 
@@ -63,8 +76,30 @@ class VenuesController < ApplicationController
       @venue = Venue.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def venue_params
-      params.require(:venue).permit(:name, :lat, :lng)
+      params.require(:venue).permit(:name, :address)
+    end
+  
+    def get_coordinates_from_address(address)
+      api_key = ENV['GOOGLE_MAPS_API_KEY']
+      base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+      encoded_address = URI.encode_www_form_component(address)
+      request_url = "#{base_url}?address=#{encoded_address}&key=#{api_key}"
+
+      response = HTTParty.get(request_url)
+  
+      if response.code == 200
+        result = JSON.parse(response.body)
+        if result["status"] == "OK"
+          location = result["results"][0]["geometry"]["location"]
+          return location["lat"], location["lng"]
+        else
+          Rails.logger.error "Geocoding failed with status: #{result["status"]}"
+          return nil
+        end
+      else
+        Rails.logger.error "HTTP Request failed with code: #{response.code}"
+        return nil
+      end
     end
 end
