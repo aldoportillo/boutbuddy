2ecfde272dad220a605523985d7ebad5332ffa2d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-16 15:47:43 -0600

    Update associations

--- app/models/weight_class.rb
+++ app/models/weight_class.rb
@@ -12,6 +12,4 @@
 class WeightClass < ApplicationRecord
   has_many :bouts, class_name: "Bout", foreign_key: "weight_class_id"
   has_many :users
-
-  has_many(:unmatched_bouts, -> {unconfirmed}, :class_name => "Bout", :foreign_key => "weight_class_id")
 end
