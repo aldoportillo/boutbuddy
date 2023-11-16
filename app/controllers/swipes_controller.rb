5aedd2edc2d86a8b82316b8753db4b21a2c01ddb
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-16 10:33:53 -0600

    Swipe officially creates an event and bout

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -1,12 +1,22 @@
 class SwipesController < ApplicationController
   def create
-    swipe = Swipe.new(swipe_params)
-    
-    if swipe.save
-      check_for_match(swipe)
-      format.html { redirect_to root_url, notice: "Bout was successfully created." }
-    else
-      # Handle error
+    @swipe = Swipe.new(swipe_params)
+
+    respond_to do |format|
+      if @swipe.save
+        # Perform any additional actions upon successful swipe creation, e.g., check for match
+        check_for_match(@swipe)
+        # Redirect to a specified path for HTML responses
+        # Replace 'some_path' with your actual redirect path, like 'fighters_carousel_path'
+        format.html { redirect_to fighters_carousel_path, notice: 'Swipe was successfully created.' }
+
+        # Return the created swipe object in JSON format for JSON responses
+        format.json { render json: @swipe, status: :created, location: @swipe }
+      else
+        # In case of save failure, render the new form or return errors
+        format.html { render :new }
+        format.json { render json: @swipe.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -25,29 +35,37 @@ class SwipesController < ApplicationController
   end
 
   def create_bout(swipe)
+    
     bout = Bout.new(red_corner_id: swipe.swiper_id, blue_corner_id: swipe.swiped_id)
     
+    debugger
     bout.event = find_suitable_event
+
+    debugger
+
+    bout.weight_class_id = 761 ##bandaid: setting weight class statically
+    
+
+    debugger
+
+
+    bout.save
   
-    if bout.save
-      # Notify users or promoters about the new bout
-      # ...
-    else
-      # Handle error
-    end
+    # if bout.save
+    #   format.html { redirect_to some_path, notice: 'Swipe was successfully created.' }
+    #     format.json { render json: @swipe, status: :created, location: @swipe }
+    # else
+    #   format.html { render :new }
+    #     format.json { render json: @swipe.errors, status: :unprocessable_entity }
+    # end
   end
 
   def find_suitable_event
-    
-    Event.find_by_sql(<<-SQL).first
-      SELECT events.*
-      FROM events
-      LEFT JOIN bouts ON bouts.event_id = events.id
-      GROUP BY events.id
-      HAVING COUNT(bouts.id) < 12
-      ORDER BY events.date ASC
-      LIMIT 1
-    SQL
+    Event.left_joins(:bouts)
+         .group('events.id')
+         .having('COUNT(bouts.id) < 12')
+         .order('events.time ASC')
+         .first
   end
 
 end
