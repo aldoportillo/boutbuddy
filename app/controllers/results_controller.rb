e8a8f70ee1c0d30c988957e2cecd3b1d0540e3e3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-25 16:06:29 -0600

    Generate new and create actions in results controller

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,28 @@
+class ResultsController < ApplicationController
+  before_action :set_event_and_bout
+
+  def new
+    @result = Result.new
+  end
+
+  def create
+    @result = @bout.results.build(result_params)
+
+    if @result.save
+      redirect_to event_path(@event), notice: 'Result was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_event_and_bout
+    @event = Event.find(params[:event_id])
+    @bout = @event.bouts.find(params[:bout_id])
+  end
+
+  def result_params
+    params.require(:result).permit(:winner_id, :victory_method, :submission_type)
+  end
+end
